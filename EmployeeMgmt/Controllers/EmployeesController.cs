06ee1ba0eea3b10d601a273c24633ea9bf5a51e6
kdk <khadkautshav7@gmail.com>
2017-03-07 12:01:01 +0545

    First Commit

---
+++ EmployeeMgmt/Controllers/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+namespace EmployeeMgmt.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (CompanyEntities entities = new CompanyEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+        public Employee  Get(int id)
+        {
+            using (CompanyEntities entities = new CompanyEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.Employee_Id == id);
+            }
+        }
+    }
+}
